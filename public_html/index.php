ec537d89d3ad221821982af7234bd74d39bc1a57
JakeTheReaper <jake.zerafa86@gmail.com>
2018-05-06 21:46:11 +1000

    Web Code

---
+++ public_html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Mind Your Own Breakfast</title>
+</head>
+
+<body>
+    <section class="section section-one">
+       <div class="main-img"><img src="img/myob-main.png" width="50%"/><br>
+       <button type="button" class="btn btn-warning btn-lg">COMING SOON</button>
+       </div>
+       
+        
+    </section>
+
+</body>
+
+</html>
